3432ad4f9eaa7bb2405dda4c57893b588cc4e323
Chris Monteferrante <cpmonte@umich.edu>
2020-03-06 21:29:23 -0500

    Update get.py

--- get.py
+++ get.py
@@ -1,7 +1,7 @@
 import requests
 
-# ~~~~~~~~~~~~~~~~~~~~~~~ Replace These 3 Lines ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-FILENAME = "Survivor"
+# ~~~~~~~~~~~~~~~~~~~~~~~ Replace These 2 Lines ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+FILENAME = "PLACEHOLDER"
 url_raw = 'https://stream-5-1.loadshare.org/stream3/VideoID-iiX3ZVR2/ekQTaZA6p205SHh8WeRhHj6Lh84-5b7g9CM7IFoakxc4WNZ_woX4XHuJO5EBLGsYQcRHHfsuu8mdIv0xCilUPZZ7GkzlIIoicNYbOWVXMcGiLVT8OJ4wiK0XTjhun9HfmcANkK_YdJfLUy7wAeNxxg/seg-!!!!-f2-v1-a1.ts?token=ip=198.0.97.237~st=1583543364~exp=1583557764~acl=/*~hmac=b00d2a075836d1ae8f68bfc0f07515cfa96d48faf64cb4eca1f9d240973666bb'
 
 file = open(FILENAME + '.ts', 'ba')
